a08a3e54869d141cd47a9d2edb687fa18c503aae
sashasobol111 <150607594+sashasobol111@users.noreply.github.com>
2025-02-03 18:30:04 +0300

    Add files via upload

--- tag_system.js
+++ tag_system.js
@@ -1,6 +1,11 @@
 const el = document.getElementById("select1");
 const tags = document.getElementsByClassName("tag");
-
+const tags1 = document.getElementsByClassName("tags1");
 el.addEventListener('change', () => {
-    
+
 })
+
+tags[0].addEventListener('click', () => {
+    tags[0].remove();
+})
+
