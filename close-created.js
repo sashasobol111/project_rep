f3b175b7d492379764a74f689237632acd3e3029
sashasobol111 <150607594+sashasobol111@users.noreply.github.com>
2025-03-05 18:36:22 +0300

    Add files via upload

--- close-created.js
+++ close-created.js
@@ -7,6 +7,22 @@ dia_cr.addEventListener('click', () => {
 })
 
 create.addEventListener('click', () => {
+    var inputValue;
+
+    const name1 = document.getElementById('name');
+    inputValue = name1.value;
+    let name = inputValue;
+    const desc1 = document.getElementById('description');
+    inputValue = desc1.value;
+    let desc = inputValue;
+    const deadl1 = document.getElementById('deadline');
+    inputValue = deadl1.value;
+    let deadl = inputValue;
+
+    const todo_list = document.getElementById('to-do-cont');
+    todo_list.innerHTML += `<div class="to-do"><div class="head_todo"><h3 class="name-to"><b>${name}</b></h3></div><div class="center_todo"><p class="desc"></p>${desc}<p class="deadl">${deadl}</p></div><div class="tagi"><div class="tags2">swss</div></div><div class="buttons_todo"><button class="edit">Edit</button><button class="complete">Comlete</button><button class="delete">Delete</button></div>  </div>`;
+
+
     window.created1.close();
 })
 
